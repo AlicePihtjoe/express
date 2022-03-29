d96a28b510f6fe3fa28b40e9158beb89792ba136
AlicePihtjoe <alice.pihtjoe@khk.ee>
2022-03-29 19:23:26 +0300

    Basic Express Application

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+// set up brouther address row router
+
+app.get('/', (req, res) => {
+    // create html response
+    res.send('<a href="/contact">Conctact Us</a> <br> <a href="/about">About Us</a>');
+});
+
+app.get('/contact', (req, res) => {
+    res.send('<h1>Conctact Us Page</h1>');
+})
+
+app.get('/about', (req, res) => {
+    res.send('<h1>About Us Page</h1>');
+})
+
+app.get('*', (req, res) => {
+    res.send('404. This page does not exist. <a href="/">Go to Homepage</a>')
+})
+//listen app via port
+app.listen(3000, () => {
+    console.log(`Server started on http://localhost:3000`)
+})
